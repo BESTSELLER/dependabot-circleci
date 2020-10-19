7e3d82ff1f8dda500d150452f0935f84530b5849
Gaardsholt <lasse.gaardsholt@bestseller.com>
2020-10-19 10:31:29 +0200

    will this work? properly not

--- circleci/docker.go
+++ circleci/docker.go
@@ -2,6 +2,7 @@ package circleci
 
 import (
 	"fmt"
+	"regexp"
 	"strings"
 
 	"github.com/google/go-containerregistry/pkg/name"
@@ -29,26 +30,6 @@ func extractImages(images []*yaml.Node) {
 	}
 }
 
-// func extractImages(images []*yaml.Node) map[string]*yaml.Node {
-// 	updates := map[string]*yaml.Node{}
-// 	for i := 0; i < len(images); i = i + 2 {
-// 		image := images[i]
-// 		if image.Value == "image" {
-// 			image = images[i+1]
-
-// 			imageVersion := findNewestDockerVersion(image.Value)
-
-// 			if image.Value != imageVersion {
-// 				oldVersion := image.Value
-// 				image.Value = imageVersion
-// 				updates[oldVersion] = image
-// 			}
-// 		}
-// 		extractImages(image.Content)
-// 	}
-// 	return updates
-// }
-
 func findNewestDockerVersion(currentVersion string) string {
 
 	fmt.Println(currentVersion)
@@ -68,11 +49,22 @@ func findNewestDockerVersion(currentVersion string) string {
 	// fix this shit
 	tags, err := getTags(currentVersion)
 	if err != nil {
-		panic(err)
+		// panic(err)
+		return currentVersion
 	}
-	newest := tags[len(tags)-1]
 
-	fmt.Println(newest)
+	versionParts := splitVersion(currentVersion)
+
+	var newTagsList []string
+	for _, tag := range tags {
+		aa := splitVersion(tag)
+
+		if aa["prefix"] == versionParts["prefix"] && aa["suffix"] == versionParts["suffix"] {
+			newTagsList = append(newTagsList, tag)
+		}
+	}
+
+	newest := newTagsList[len(newTagsList)-1]
 
 	return newest
 }
@@ -96,4 +88,28 @@ func getTags(circleciTag string) ([]string, error) {
 	}
 
 	return tags.Tags, nil
+}
+
+func splitVersion(version string) map[string]string {
+	// Regex stolen with love from dependabot-core
+	// https://github.com/dependabot/dependabot-core/blob/v0.123.0/docker/lib/dependabot/docker/update_checker.rb#L15-L27
+	versionRegex := `v?(?P<version>[0-9]+(?:(?:\.[a-z0-9]+)|(?:-(?:kb)?[0-9]+))*)`
+	versionWithSFX := versionRegex + `(?P<suffix>-[a-z0-9.\-]+)?$`
+	versionWithPFX := `(?P<prefix>[a-z0-9.\-]+-)?` + versionRegex + `$`
+	versionWithPFXSFX := `(?P<prefix>[a-z\-]+-)?` + versionRegex + `(?P<suffix>-[a-z\-]+)?$`
+
+	nameWithVersion := versionWithPFX + `|` + versionWithSFX + `|` + versionWithPFXSFX
+
+	var myExp = regexp.MustCompile(nameWithVersion)
+	match := myExp.FindStringSubmatch(version)
+	result := make(map[string]string)
+
+	matches := myExp.SubexpNames()
+	for i, name := range matches {
+		if i != 0 && name != "" && match[i] != "" {
+			result[name] = match[i]
+		}
+	}
+
+	return result
 }
