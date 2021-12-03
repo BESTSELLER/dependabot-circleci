23d27077a9e8fb0f95a35b7c976832d572549254
Christian Grøntved Andersen <christian.andersen@bestseller.com>
2021-12-03 15:30:08 +0100

    Changed logging to method

--- logger/logger.go
+++ logger/logger.go
@@ -1,23 +1,19 @@
 package logger
 
 import (
-	"strings"
-
 	"github.com/BESTSELLER/dependabot-circleci/config"
 	"github.com/rs/zerolog"
 )
 
 // Init sets the global loglevel
 func Init() {
-	// default is info
-	zerolog.SetGlobalLevel(zerolog.WarnLevel)
 
-	if config.EnvVars.LogLevel == "" {
+	if config.EnvVars.LogLevel == nil {
+		zerolog.SetGlobalLevel(zerolog.InfoLevel)
 		return
 	}
-	if strings.ToLower(config.EnvVars.LogLevel) == "debug" {
-		zerolog.SetGlobalLevel(zerolog.DebugLevel)
-	} else if strings.ToLower(config.EnvVars.LogLevel) == "info" {
-		zerolog.SetGlobalLevel(zerolog.InfoLevel)
+
+	if *config.EnvVars.LogLevel <= int(zerolog.Disabled) {
+		zerolog.SetGlobalLevel(zerolog.Level(*config.EnvVars.LogLevel))
 	}
 }
