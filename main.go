6f2cbe6dc3195036f3d43d1f507a8fdd7b2920c1
Christian Grøntved Andersen <christian.andersen@bestseller.com>
2021-12-06 13:03:03 +0100

    test setup

--- main.go
+++ main.go
@@ -1,52 +1,50 @@
 package main
 
 import (
-	"io/ioutil"
-	"os"
 	"sync"
 
 	"github.com/rs/zerolog/log"
 
 	"github.com/BESTSELLER/dependabot-circleci/api"
 	"github.com/BESTSELLER/dependabot-circleci/logger"
-	"github.com/BESTSELLER/go-vault/gcpss"
 
 	"github.com/BESTSELLER/dependabot-circleci/config"
 )
 
 var wg sync.WaitGroup
 
-func init() {
-	vaultAddr := os.Getenv("VAULT_ADDR")
-	if vaultAddr == "" {
-		log.Fatal().Msg("VAULT_ADDR must be set")
-	}
-	vaultSecret := os.Getenv("VAULT_SECRET")
-	if vaultSecret == "" {
-		log.Fatal().Msg("VAULT_SECRET must be set")
-	}
-	vaultRole := os.Getenv("VAULT_ROLE")
-	if vaultRole == "" {
-		log.Fatal().Msg("VAULT_ROLE must be set")
-	}
+// TODO remeber to uncomment
+// func init() {
+// 	vaultAddr := os.Getenv("VAULT_ADDR")
+// 	if vaultAddr == "" {
+// 		log.Fatal().Msg("VAULT_ADDR must be set")
+// 	}
+// 	vaultSecret := os.Getenv("VAULT_SECRET")
+// 	if vaultSecret == "" {
+// 		log.Fatal().Msg("VAULT_SECRET must be set")
+// 	}
+// 	vaultRole := os.Getenv("VAULT_ROLE")
+// 	if vaultRole == "" {
+// 		log.Fatal().Msg("VAULT_ROLE must be set")
+// 	}
 
-	secret, err := gcpss.FetchVaultSecret(vaultAddr, vaultSecret, vaultRole)
-	if err != nil {
-		log.Fatal().Err(err)
-	}
+// 	secret, err := gcpss.FetchVaultSecret(vaultAddr, vaultSecret, vaultRole)
+// 	if err != nil {
+// 		log.Fatal().Err(err)
+// 	}
 
-	err = os.Mkdir("/secrets", 0644)
-	if err != nil {
-		log.Fatal().Err(err).Msg("unable to create secrets dir")
-	}
+// 	err = os.Mkdir("/secrets", 0644)
+// 	if err != nil {
+// 		log.Fatal().Err(err).Msg("unable to create secrets dir")
+// 	}
 
-	data := []byte(secret)
-	err = ioutil.WriteFile("/secrets/secrets", data, 0644)
-	if err != nil {
-		log.Fatal().Err(err).Msg("unable to write secrets")
-	}
+// 	data := []byte(secret)
+// 	err = ioutil.WriteFile("/secrets/secrets", data, 0644)
+// 	if err != nil {
+// 		log.Fatal().Err(err).Msg("unable to write secrets")
+// 	}
 
-}
+// }
 
 func main() {
 	err := config.LoadEnvConfig()
