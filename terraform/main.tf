da6e6a093c7b1bec45fd984bc17513593d51f40d
Brett Wright <wright.brett@gmail.com>
2022-06-20 13:17:46 +0200

    added controller, webhook and workers

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,4 @@
+
 module "bq" {
   source     = "./modules/bq"
   project_id = var.project_id
@@ -5,18 +6,46 @@ module "bq" {
   labels     = var.labels
 }
 
-module "cloud_run" {
-  source     = "./modules/cloud_run"
+module "controller" {
+  source = "./modules/cloud_run"
+  args   = ["-controller"]
+  scaling = {
+    max = "1"
+    min = "0"
+  }
   project_id = var.project_id
   location   = "europe-west4"
   labels     = var.labels
   tag        = var.tag
 }
 
+module "worker" {
+  source = "./modules/cloud_run"
+  args   = ["-worker"]
+  scaling = {
+    max = "1"
+    min = "0"
+  }
+  project_id = var.project_id
+  location   = "europe-west4"
+  labels     = var.labels
+  tag        = var.tag
+}
+
+module "webhook" {
+  source     = "./modules/cloud_run"
+  args       = ["-webhook"]
+  project_id = var.project_id
+  location   = "europe-west4"
+  labels     = var.labels
+  tag        = var.tag
+}
+
+
 module "schedule" {
   source     = "./modules/schedule"
   project_id = var.project_id
-  url        = module.cloud_run.url
+  url        = module.controller.url
   location   = "europe-west4"
   labels     = var.labels
 }
