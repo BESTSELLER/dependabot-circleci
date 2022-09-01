e2a7b904b87993e0bbe9184b090a78713b7f9628
Peter Brøndum <peter.brondum@bestseller.com>
2022-09-01 11:19:12 +0200

    trying to fix log sink filter

--- terraform/modules/logs/main.tf
+++ terraform/modules/logs/main.tf
@@ -4,7 +4,7 @@ resource "google_logging_project_sink" "cloud-run" {
   description            = "Export logs from all Cloud Run Services to datadog"
   unique_writer_identity = true
   destination            = "pubsub.googleapis.com/projects/${var.monitor_project_id}/topics/export-logs-to-datadog"
-  filter                 = "resource.type = 'cloud_run_revision' OR resource.type = 'cloud_run_job' severity>=DEFAULT"
+  filter                 = "resource.type = cloud_run_revision OR resource.type = cloud_run_job severity>=DEFAULT"
 }
 
 resource "google_project_iam_binding" "pubsub_publish" {
