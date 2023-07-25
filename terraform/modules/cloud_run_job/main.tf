82fbed86df8b9993f0d643c82de6f19f53ed2ffe
Peter Brøndum <peter.brondum@bestseller.com>
2023-07-25 11:31:05 +0200

    nested template, for some reason ?? wtf gcp

--- terraform/modules/cloud_run_job/main.tf
+++ terraform/modules/cloud_run_job/main.tf
@@ -11,116 +11,113 @@ resource "google_cloud_run_v2_job" "main" {
     version = replace(var.tag, ".", "_")
   }
   template {
-    service_account = "${var.service}-v3@${var.project_id}.iam.gserviceaccount.com"
-    timeout         = "3600s"
-    labels = {
-      env     = var.env
-      service = var.service
-      team    = var.team
-      version = replace(var.tag, ".", "_")
-    }
-    containers {
-      name       = var.name
-      image      = "europe-docker.pkg.dev/artifacts-pub-prod-b57f/public-docker/${var.service}:${var.tag}"
-      args       = var.args
-      depends_on = ["secret-dumper"]
-      env {
-        name  = "DEPENDABOT_WORKERURL"
-        value = var.worker_url
+    template {
+      service_account = "${var.service}-v3@${var.project_id}.iam.gserviceaccount.com"
+      timeout         = "3600s"
+      labels = {
+        env     = var.env
+        service = var.service
+        team    = var.team
+        version = replace(var.tag, ".", "_")
       }
-      env {
-        name  = "DEPENDABOT_CONFIG"
-        value = "/secrets/app-secrets"
-      }
-      env {
-        name  = "DEPENDABOT_DBCONFIG"
-        value = "/secrets/db-secrets"
-      }
-      volume_mounts {
-        name       = "secrets"
-        mount_path = "/secrets"
-      }
-      volume_mounts {
-        name       = "cloudsql"
-        mount_path = "/cloudsql"
-      }
-    }
-    containers {
-      name  = "secret-dumper"
-      image = "europe-docker.pkg.dev/artifacts-pub-prod-b57f/public-docker/harpocrates:2.4.0"
-      args = [
-        jsonencode({
-          "format" : "json",
-          "output" : "/secrets",
-          "secrets" : [
-            {
-              "ES/data/${var.service}/prod" : {
-                "filename" : "app-secrets"
-              }
-            },
-            {
-              "ES/data/${var.service}/db" : {
-                "filename" : "db-secrets"
-              }
-            }
-          ]
-        })
-      ]
-      env {
-        name  = "VAULT_ADDR"
-        value = "https://vault.bestsellerit.com"
-      }
-      env {
-        name  = "AUTH_NAME"
-        value = "dependabot-circleci-v3"
-      }
-      env {
-        name  = "ROLE_NAME"
-        value = "dependabot-circleci-v3"
-      }
-      env {
-        name  = "GCP_WORKLOAD_ID"
-        value = "true"
-      }
-      env {
-        name  = "CONTINUOUS"
-        value = "true"
-      }
-      env {
-        name  = "INTERVAL"
-        value = "60s"
-      }
-      env {
-        name  = "LOG_LEVEL"
-        value = "warn"
-      }
-      volume_mounts {
-        name       = "secrets"
-        mount_path = "/secrets"
-      }
-      startup_probe {
-        http_get {
-          path = "/status"
-          port = 8000
+      containers {
+        name       = var.name
+        image      = "europe-docker.pkg.dev/artifacts-pub-prod-b57f/public-docker/${var.service}:${var.tag}"
+        args       = var.args
+        depends_on = ["secret-dumper"]
+        env {
+          name  = "DEPENDABOT_WORKERURL"
+          value = var.worker_url
         }
-        initial_delay_seconds = 2
+        env {
+          name  = "DEPENDABOT_CONFIG"
+          value = "/secrets/app-secrets"
+        }
+        env {
+          name  = "DEPENDABOT_DBCONFIG"
+          value = "/secrets/db-secrets"
+        }
+        volume_mounts {
+          name       = "secrets"
+          mount_path = "/secrets"
+        }
+        volume_mounts {
+          name       = "cloudsql"
+          mount_path = "/cloudsql"
+        }
+      }
+      containers {
+        name  = "secret-dumper"
+        image = "europe-docker.pkg.dev/artifacts-pub-prod-b57f/public-docker/harpocrates:2.4.0"
+        args = [
+          jsonencode({
+            "format" : "json",
+            "output" : "/secrets",
+            "secrets" : [
+              {
+                "ES/data/${var.service}/prod" : {
+                  "filename" : "app-secrets"
+                }
+              },
+              {
+                "ES/data/${var.service}/db" : {
+                  "filename" : "db-secrets"
+                }
+              }
+            ]
+          })
+        ]
+        env {
+          name  = "VAULT_ADDR"
+          value = "https://vault.bestsellerit.com"
+        }
+        env {
+          name  = "AUTH_NAME"
+          value = "dependabot-circleci-v3"
+        }
+        env {
+          name  = "ROLE_NAME"
+          value = "dependabot-circleci-v3"
+        }
+        env {
+          name  = "GCP_WORKLOAD_ID"
+          value = "true"
+        }
+        env {
+          name  = "CONTINUOUS"
+          value = "true"
+        }
+        env {
+          name  = "INTERVAL"
+          value = "60s"
+        }
+        env {
+          name  = "LOG_LEVEL"
+          value = "warn"
+        }
+        volume_mounts {
+          name       = "secrets"
+          mount_path = "/secrets"
+        }
+        startup_probe {
+          http_get {
+            path = "/status"
+            port = 8000
+          }
+          initial_delay_seconds = 2
+        }
+      }
+      volumes {
+        name = "secrets"
+        empty_dir {}
+      }
+      volumes {
+        name = "cloudsql"
+        cloud_sql_instance {
+          instances = [var.db_instance]
+        }
       }
     }
-    volumes {
-      name = "secrets"
-      empty_dir {}
-    }
-    volumes {
-      name = "cloudsql"
-      cloud_sql_instance {
-        instances = [var.db_instance]
-      }
-    }
-  }
-
-  traffic {
-    type    = "TRAFFIC_TARGET_ALLOCATION_TYPE_LATEST"
-    percent = 100
   }
 }
 
