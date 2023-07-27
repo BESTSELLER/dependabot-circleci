d204dcbf174ec2fb63904b1e8da10d971f52570c
Peter Brøndum <peter.brondum@bestseller.com>
2023-07-27 14:48:27 +0200

    improve logger test

--- logger/logger_test.go
+++ logger/logger_test.go
@@ -1,10 +1,9 @@
-package logger_test
+package logger
 
 import (
 	"testing"
 
 	"github.com/BESTSELLER/dependabot-circleci/config"
-	"github.com/BESTSELLER/dependabot-circleci/logger"
 	"github.com/rs/zerolog"
 	"github.com/stretchr/testify/assert"
 )
@@ -14,20 +13,20 @@ func TestInit(t *testing.T) {
 	// Test case 1: both AppConfig and EnvVars are nil
 	config.AppConfig.LogLevel = nil
 	config.EnvVars.LogLevel = nil
-	logger.Init()
-	assert.Equal(t, zerolog.InfoLevel, logger.LogLevel)
+	Init()
+	assert.Equal(t, zerolog.InfoLevel, LogLevel)
 
 	// Test case 2: EnvVars is set to Disabled
 	config.AppConfig.LogLevel = nil
 	config.EnvVars.LogLevel = new(int)
 	*config.EnvVars.LogLevel = int(zerolog.Disabled)
-	logger.Init()
-	assert.Equal(t, zerolog.Disabled, logger.LogLevel)
+	Init()
+	assert.Equal(t, zerolog.Disabled, LogLevel)
 
 	// Test case 3: AppConfig is set to Error
 	config.AppConfig.LogLevel = new(int)
 	*config.AppConfig.LogLevel = int(zerolog.ErrorLevel)
 	config.EnvVars.LogLevel = nil
-	logger.Init()
-	assert.Equal(t, zerolog.ErrorLevel, logger.LogLevel)
+	Init()
+	assert.Equal(t, zerolog.ErrorLevel, LogLevel)
 }
