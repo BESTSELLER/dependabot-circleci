fd5501a9429868896acf7e22d1ce9c4a43cd49a1
Andrei Predoiu <andrei.predoiu@bestseller.com>
2024-04-05 10:56:24 +0200

    ocd related stuff

--- config/config.go
+++ config/config.go
@@ -33,7 +33,7 @@ type BestsellerSpecificConfig struct {
 	Running bool
 }
 
-// DBConfig contains global db config
+// DBConfigSpec contains global db config
 type DBConfigSpec struct {
 	ConnectionName   string `yaml:"connection_name"`
 	ConnectionString string `yaml:"connection_string"`
@@ -43,7 +43,7 @@ type DBConfigSpec struct {
 	Username         string `yaml:"username"`
 }
 
-// RepoConfig contains specific config for each repos
+// RepoConfig contains specific config for each repo
 type RepoConfig struct {
 	TargetBranch string   `yaml:"target-branch,omitempty"`
 	Reviewers    []string `yaml:"reviewers,omitempty"`
@@ -84,7 +84,7 @@ func ReadDBConfig(secrets []byte) error {
 // ReadRepoConfig reads a yaml file
 func ReadRepoConfig(content []byte) (*RepoConfig, error) {
 	// default values setup here
-	repoConfig := RepoConfig{Directory: "/.circleci", Schedule: "daily"}
+	repoConfig := RepoConfig{Directory: "/.circleci/config.yml", Schedule: "daily"}
 
 	if err := yaml.UnmarshalStrict(content, &repoConfig); err != nil {
 		return nil, errors.Wrap(err, "failed parsing repository configuration file")
