c5b3b42588ac0df0a57140893a2701a2428ba87c
Andrei Predoiu <andrei.predoiu@bestseller.com>
2024-05-16 09:24:26 +0200

    fml

--- terraform/modules/cloud_run/main.tf
+++ terraform/modules/cloud_run/main.tf
@@ -125,7 +125,7 @@ resource "google_cloud_run_v2_service" "main" {
     volumes {
       name = "secrets"
       empty_dir {
-        medium = "Memory"
+        medium = "MEMORY"
         size_limit = "1Mi"
       }
     }
